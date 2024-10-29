b4c6db8d556d454d0cb7189099b9951c14fa04b8
Ryan Edge <ryan.c.edge@outlook.com>
2024-10-29 12:12:56 -0400

    Remove finished code

--- notes_api/services/notes.dart
+++ notes_api/services/notes.dart
@@ -1,85 +1,27 @@
 import 'package:nitric_sdk/nitric.dart';
 
-import '../common/sql_query_builder.dart';
-import '../models/note.dart';
-import '../resources/db.dart';
-
 void main() async {
-  final connection = await getConnection();
-
   final helloApi = Nitric.api("main");
 
   helloApi.get("/notes", (ctx) async {
-    final query = SQLQueryBuilder()
-      ..select(['id', 'title', 'content'])
-      ..from('notes');
-
-    final rows = await query.execute(connection);
-
-    final notes = rows.map((row) => Note.fromMap(row.toColumnMap())).toList();
-
-    ctx.res.json({'notes': notes.map((note) => note.toMap()).toList()});
+    ctx.res.json({'notes': []});
 
     return ctx;
   });
 
   helloApi.post("/notes", (ctx) async {
-    final newNote = CreateNoteBody.fromMap(ctx.req.json());
-
-    final query = SQLQueryBuilder()..insert('notes', newNote.toMap());
-    final rows = await query.execute(connection);
-
-    final notes = rows.map((row) => Note.fromMap(row.toColumnMap())).toList();
-
-    ctx.res.json({'notes': notes.map((note) => note.toMap()).toList()});
+    ctx.res.json({'notes': []});
 
     return ctx;
   });
 
   helloApi.put("/notes/:id", (ctx) async {
-    final idArg = ctx.req.pathParams["id"]!;
-    final id = int.tryParse(idArg);
-
-    if (id == null) {
-      ctx.res.status = 400;
-      return ctx;
-    }
-
-    final updatedNote = UpdateNoteBody.fromMap(ctx.req.json());
-
-    final updateQuery = SQLQueryBuilder()
-      ..update('notes', updatedNote.toMap())
-      ..where('id = ', id);
-    await updateQuery.execute(connection);
-
-    final selectQuery = SQLQueryBuilder()
-      ..select(['id', 'title', 'content'])
-      ..from('notes')
-      ..where('id =', id);
-
-    final rows = await selectQuery.execute(connection);
-
-    final notes = rows.map((row) => Note.fromMap(row.toColumnMap())).toList();
-
-    ctx.res.json({'notes': notes.map((note) => note.toMap()).toList()});
+    ctx.res.json({'notes': []});
 
     return ctx;
   });
 
   helloApi.delete("/notes/:id", (ctx) async {
-    final idArg = ctx.req.pathParams["id"]!;
-    final id = int.tryParse(idArg);
-
-    if (id == null) {
-      ctx.res.status = 400;
-      return ctx;
-    }
-
-    final query = SQLQueryBuilder()
-      ..deleteFrom('notes')
-      ..where('id =', id);
-    await query.execute(connection);
-
     ctx.res.status = 204;
 
     return ctx;
